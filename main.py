a1021a8ff9c157c53ce6db140da583dc91d075fd
N.Bhuvana Pravallika <85564678+nbpravallika@users.noreply.github.com>
2021-11-07 23:36:46 -0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+def check_palindrome(str1):
+  if str1[::-1]==str1:
+            return True
+  return False
+str1=input()
+if check_palindrome==str1:
+     print("palindrome")
+else:
+     print("non palindrome")
+
